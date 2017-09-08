de03456120c8cf14ae05145df3c33ae870de0c8a
Prys Williams <pryswilliams@gmail.com>
2017-09-08 09:49:43 +0100

    Changed target

--- check.py
+++ check.py
@@ -4,7 +4,7 @@ import urllib
 var = 1
 while var==1:
    try:
-      print urllib.urlopen("http://quotes.prys.me.uk:3412/quote/random").getcode()
+      print urllib.urlopen("http://minion1:31378/quote/random").getcode()
    except IOError:
       print "Error accessing website"
    time.sleep(5)
